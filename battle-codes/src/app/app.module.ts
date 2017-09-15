49892b8e47864943973b1609508c483609876adb
Brian Mann <bmann505@gamil.com>
2017-09-15 11:05:21 -0600

    footer

--- battle-codes/src/app/app.module.ts
+++ battle-codes/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
