02d5b36e1f34c8e812ba3853f1eccfea8c617897
Brian Mann <bmann505@gamil.com>
2017-09-15 10:54:23 -0600

    about

--- battle-codes/src/app/app.module.ts
+++ battle-codes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
