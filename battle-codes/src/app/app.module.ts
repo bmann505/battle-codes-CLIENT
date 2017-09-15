91c66e0c6232d571a2ff09742e3b94062cecf748
Brian Mann <bmann505@gamil.com>
2017-09-15 11:16:31 -0600

    admin email component

--- battle-codes/src/app/app.module.ts
+++ battle-codes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
+import { AdminSignInComponent } from './header/admin-sign-in/admin-sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    FooterComponent
+    FooterComponent,
+    AdminSignInComponent
   ],
   imports: [
     BrowserModule
