0c7ee5b4c75250e280f91dfadc0cbbc52701d59b
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-09-18 12:06:47 -0600

    app module fixed with correct path routes

--- battle-codes/src/app/app.module.ts
+++ battle-codes/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './home/header/header.component';
+import { AboutComponent } from './home/about/about.component';
 import { FooterComponent } from './footer/footer.component';
-import { AdminSignInComponent } from './header/admin-sign-in/admin-sign-in.component';
-import { AdminSignUpComponent } from './header/admin-sign-in/admin-sign-up/admin-sign-up.component';
-import { TeamSignInComponent } from './header/team-sign-in/team-sign-in.component';
+import { AdminSignInComponent } from './home/header/admin-sign-in/admin-sign-in.component';
+import { AdminSignUpComponent } from './home/header/admin-sign-in/admin-sign-up/admin-sign-up.component';
+import { TeamSignInComponent } from './home/header/team-sign-in/team-sign-in.component';
 import { RoomComponent } from './room/room.component';
 import { RoomHeaderComponent } from './room/room-header/room-header.component';
 import { ScoreboardComponent } from './room/scoreboard/scoreboard.component';
@@ -18,7 +18,7 @@ import { QuestionListComponent } from './room/question-list/question-list.compon
 import { PostedQuestionComponent } from './room/posted-question/posted-question.component';
 import { FinishGameComponent } from './room/finish-game/finish-game.component';
 import { TeamAnswerComponent } from './room/team-answer/team-answer.component';
-import { CreateRoomComponent } from './create-room/create-room.component';
+import { CreateRoomComponent } from './home/create-room/create-room.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 
