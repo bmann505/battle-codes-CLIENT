09eec57b032a3cd481fa6dcfc0948aa026c6015f
yousif al timimie <yaltimimie@gmail.com>
2017-09-20 00:52:53 -0600

    scores and board

--- battle-codes/src/app/home/room/scoreboard/scoreboard.component.ts
+++ battle-codes/src/app/home/room/scoreboard/scoreboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { roomService } from 'app/room.service';
+import * as io from 'socket.io-client'
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
   styleUrls: ['./scoreboard.component.css']
 })
 export class ScoreboardComponent implements OnInit {
-
-  constructor() { }
-
+    socket=io('http://localhost:3000')
+  constructor(private roomService: roomService) { }
+teams=this.roomService.teams
   ngOnInit() {
+
+
+this.roomService.resDataFromTeam(this.socket)
   }
+
+
 
 }
--- battle-codes/src/app/home/room/team-room/team-answer/team-answer.component.ts
+++ battle-codes/src/app/home/room/team-room/team-answer/team-answer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { roomService } from 'app/room.service';
+import * as io from 'socket.io-client'
 @Component({
   selector: 'app-team-answer',
   templateUrl: './team-answer.component.html',
   styleUrls: ['./team-answer.component.css']
 })
 export class TeamAnswerComponent implements OnInit {
-
-  constructor() { }
+  socket=io('http://localhost:3000')
+  constructor(private roomService: roomService) { }
 
   ngOnInit() {
   }
-
+ emitAnswer(answer){
+   let data = {
+   room: this.roomService.room.name,
+   handle: this.roomService.team.name,
+   message:answer,
+   question: "",
+   score:0
+ }
+ console.log(data);
+ this.socket.emit('room', data);
+ }
 }
