062bcda24bbd77e6c2a23851b974a1e26bc1ec40
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-09-21 12:47:20 -0600

    delete extra grading component file

--- battle-codes/src/app/auth.service.ts
+++ battle-codes/src/app/auth.service.ts
@@ -1,6 +1,10 @@
 export class AuthService {
 
  signUp(name: string, email: string, password: string) {
+  // let name = form.value.name
+  // let email = form.value.email;
+  // let password = form.value.password;
+    console.log("data")
       const post = {
         "name": name,
         "email": email,
@@ -13,39 +17,16 @@ export class AuthService {
         },
         body: JSON.stringify(post)
       }
-       fetch('http://localhost:3000/signup', settings)
-       .then((data) => {
-        return data.json()
+
+       fetch('http://localhost:3000/signup', settings).then((data) => {
+        console.log(data)
       })
-      .then(res => {
-        localStorage.setItem('token', res.data)
-      })
+
+      // const refresh = await fetch('http://localhost:3000/signup')
+      // const res = await refresh.json()
+
+      // localStorage.setItem('token', res.data)
+      // console.log(localStorage.getItem('token'))
+      // form.reset();
     }
-
-signIn(email: string, password: string) {
-  const post = {
-    "email": email,
-    "password": password
-  }
-  const settings = {
-    method: "POST",
-    headers: {
-      "content-type": "application/json"
-    },
-    body: JSON.stringify(post)
-  }
-
-   fetch('http://localhost:3000/signin', settings)
-   .then((data) => {
-    return data.json()
-  })
-  .then(res => {
-    if(res.error) {
-      alert(res.error)
-    } else {
-    localStorage.setItem('token', res.data)
-  }
-  })
-}
-
   }
